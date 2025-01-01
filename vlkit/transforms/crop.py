a01c5c0a9e67de8fd0b80d262f8b2dd8ecd6a16f
Batchor <batchfy@gmail.com>
2025-01-01 10:47:51 -0800

    crop along long side

--- vlkit/transforms/crop.py
+++ vlkit/transforms/crop.py
@@ -1,9 +1,52 @@
 import numpy as np
 from ..array import isarray
 from PIL import Image
+from typing import Union
 
 
-def crop_image(x, top: int, left: int, target_h: int, target_w: int, is_hw_first: bool = True):
+def get_input_shape(x: Union[np.ndarray, Image.Image, list], is_hw_first: bool = True):
+    """
+    Get the input image shape.
+
+    Parameters:
+    x (Union[np.ndarray, torch.Tensor, Image.Image, list]): The input image(s). It can be a NumPy array, 
+        PyTorch tensor, PIL.Image, or a list of these types.
+    is_hw_first (bool, optional): If True, the shape is returned as (height, width). If False, the shape 
+        is returned as (width, height). Default is True.
+
+    Returns:
+    tuple: A tuple containing the height and width of the input image(s).
+
+    Raises:
+    TypeError: If the input is not a NumPy array, PyTorch tensor, PIL.Image, or list of these types.
+    AssertionError: If the input is a list of images and they do not all have the same size.
+    """
+    if isinstance(x, Image.Image):
+        input_w, input_h = x.size
+    elif isarray(x):
+        input_h, input_w = (x.shape[:2] if is_hw_first else x.shape[-2:])
+    elif isinstance(x, list):
+        assert len(x) > 0
+        if isarray(x[0]):
+            input_h, input_w = (x[0].shape[:2] if is_hw_first else x[0].shape[-2:])
+            for x1 in x:
+                assert (input_h, input_w) == x1.shape[:2]
+        elif isinstance(x[0], Image.Image):
+            input_w, input_h = x[0].size
+            for x1 in x:
+                assert (input_w, input_h) == x1.size, "All images must have the same size."
+        else:
+            raise TypeError("Input list must contain NumPy arrays or PIL.Images.")
+    else:
+        raise TypeError("Input must be a NumPy array, PyTorch tensor, an PIL.Image, or list of them.")
+    return input_h, input_w
+
+
+def crop_image(
+    x: Union[np.ndarray, Image.Image],
+    top: int, left: int, target_h: int, target_w: int, 
+    is_hw_first: bool = True
+    ) -> Union[np.ndarray, Image.Image]:
     """
     Crop an image or an array to the specified dimensions.
 
@@ -14,8 +57,8 @@ def crop_image(x, top: int, left: int, target_h: int, target_w: int, is_hw_first
     target_h (int): The height of the cropped area.
     target_w (int): The width of the cropped area.
     is_hw_first (bool, optional): If True, the array is assumed to have the shape (height, width, ...).
-                                    If False, the array is assumed to have the shape (..., height, width).
-                                    Default is True.
+                  If False, the array is assumed to have the shape (..., height, width).
+                  Default is True.
 
     Returns:
     PIL.Image.Image or numpy.ndarray: The cropped image or array.
@@ -30,8 +73,7 @@ def crop_image(x, top: int, left: int, target_h: int, target_w: int, is_hw_first
     return cropped
 
 
-
-def random_crop(x, size: tuple, is_hw_first=True):
+def random_crop(x: Union[np.ndarray, Image.Image, list], size: tuple, is_hw_first=True):
     """
     Perform a random crop on the input tensor.
     
@@ -52,24 +94,7 @@ def random_crop(x, size: tuple, is_hw_first=True):
     assert isarray(x) or isinstance(x, (Image.Image, list))
 
     target_h, target_w = size
-    if isinstance(x, Image.Image):
-        input_w, input_h = x.size
-    elif isarray(x):
-        input_h, input_w = (x.shape[:2] if is_hw_first else x.shape[-2:])
-    elif isinstance(x, list):
-        assert len(x) > 0
-        if isarray(x[0]):
-            input_h, input_w = (x[0].shape[:2] if is_hw_first else x[0].shape[-2:])
-            for x1 in x:
-                assert (input_h, input_w) == x1.shape[:2]
-        elif isinstance(x[0], Image.Image):
-            input_w, input_h = x[0].size
-            for x1 in x:
-                assert (input_w, input_h) == x1.size, "All images must have the same size."
-        else:
-            raise TypeError("Input list must contain NumPy arrays or PIL.Images.")
-    else:
-        raise TypeError("Input must be a NumPy array, PyTorch tensor, an PIL.Image, or list of them.")
+    input_h, input_w = get_input_shape(x, is_hw_first)
 
     if target_h > input_h or target_w > input_w:
         raise ValueError("Target size cannot be larger than the input size.")
@@ -85,23 +110,31 @@ def random_crop(x, size: tuple, is_hw_first=True):
     return cropped
 
 
-def crop_long_edge(img, mode:str='center'):
+def crop_long_edge(
+        x: Union[np.ndarray, Image.Image, list],
+        mode:str='center',
+        is_hw_first:bool=True
+) -> Union[np.ndarray, Image.Image]:
     assert mode in ['center', 'random'], \
             f"mode must be either 'random' or 'center'."
-    img_height, img_width = img.shape[:2]
-    crop_size = min(img_height, img_width)
+    assert isarray(x) or isinstance(x, (Image.Image, list))
+    input_h, input_w = get_input_shape(x, is_hw_first)
+    crop_size = min(input_h, input_w)
     if mode == 'random':
-        y1 = 0 if img_height == crop_size else \
-            np.random.randint(0, img_height - crop_size)
-        x1 = 0 if img_width == crop_size else \
-            np.random.randint(0, img_width - crop_size)
+        top = 0 if input_h == crop_size else \
+            np.random.randint(0, input_h - crop_size)
+        left = 0 if input_w == crop_size else \
+            np.random.randint(0, input_w - crop_size)
     else:
-        y1 = 0 if img_height == crop_size else \
-            int((img_height - crop_size) // 2)
-        x1 = 0 if img_width == crop_size else \
-            int((img_width - crop_size) // 2)
-    y2, x2 = y1 + crop_size, x1 + crop_size
-    return img[y1:y2, x1:x2]
+        top = 0 if input_h == crop_size else \
+            int((input_h - crop_size) // 2)
+        left = 0 if input_w == crop_size else \
+            int((input_w - crop_size) // 2)
+    if isinstance(x, list):
+        cropped = [crop_image(x1, top, left, crop_size, crop_size, is_hw_first) for x1 in x]
+    else:
+        cropped = crop_image(x, top, left, crop_size, crop_size, is_hw_first)
+    return cropped
 
 
 def center_crop(image: np.ndarray, target_size, is_hw_first:bool) -> np.ndarray:
